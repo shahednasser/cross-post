4eba76339ef4f89b18a9fed6f073c475a19520ef
Jean-Baptiste Rocher <jeanb.rocher@gmail.com>
2021-11-28 17:25:18 +0100

    style(prettier): Run prettier with ballbark settings

--- src/commands/run.js
+++ src/commands/run.js
@@ -1,13 +1,20 @@
 const Conf = require('conf')
 const got = require('got')
-const jsdom = require("jsdom")
+const jsdom = require('jsdom')
 const { JSDOM } = jsdom
 const { NodeHtmlMarkdown } = require('node-html-markdown')
 const html2markdown = new NodeHtmlMarkdown()
 const CLI = require('clui')
 const Spinner = CLI.Spinner
-const { allowedPlatforms, displayError, displaySuccess, isPlatformAllowed, platformNotAllowedMessage, isDataURL, imagePlatform } = 
-    require('../utils')
+const {
+    allowedPlatforms,
+    displayError,
+    displaySuccess,
+    isPlatformAllowed,
+    platformNotAllowedMessage,
+    isDataURL,
+    imagePlatform,
+} = require('../utils')
 const postToDev = require('./platforms/dev')
 const postToHashnode = require('./platforms/hashnode')
 const postToMedium = require('./platforms/medium')
@@ -20,13 +27,16 @@ let platformsPosted = 0, //incremental count of platforms the article is posted 
     chosenPlatforms = allowedPlatforms //the platforms chosen, defaults to all platforms
 
 /**
- * 
+ *
  * @param {string} url URL of the blog post
  * @param {object} param1 The parameters from the command line
  */
-function run (url, {title, platforms, selector, public, ignoreImage, imageSelector, imageUrl}) {
-    if (typeof public !== "boolean") {
-        public = false;
+function run(
+    url,
+    { title, platforms, selector, public, ignoreImage, imageSelector, imageUrl }
+) {
+    if (typeof public !== 'boolean') {
+        public = false
     }
 
     //check if all platforms chosen are correct
@@ -36,9 +46,7 @@ function run (url, {title, platforms, selector, public, ignoreImage, imageSelect
         })
         if (error) {
             //if some of the platforms are not correct, return
-            console.error(
-                displayError(platformNotAllowedMessage)
-            )
+            console.error(displayError(platformNotAllowedMessage))
             return
         }
         //set chosen platforms to platforms chosen
@@ -55,7 +63,9 @@ function run (url, {title, platforms, selector, public, ignoreImage, imageSelect
 
     if (errorPlatform) {
         console.error(
-            displayError(`Please set the configurations required for ${errorPlatform}`)
+            displayError(
+                `Please set the configurations required for ${errorPlatform}`
+            )
         )
         return
     }
@@ -70,82 +80,104 @@ function run (url, {title, platforms, selector, public, ignoreImage, imageSelect
 
     //start loading
     loading.start()
-    got(url).then(async (response) => {
-        const dom = new JSDOM(response.body, {
-                resources: 'usable',
-                includeNodeLocations: true
-            }),
-            articleNode = dom.window.document.querySelector(selector)
-        if (articleNode) {
-            //if article element found, get its HTML content
-            const html = articleNode.innerHTML
-            let markdown = ""
-            if (html) {
-                markdown = html2markdown.translate(html)
-            }
-            //check if title is set in the command line arguments
-            if (!title) {
-                //get title of article
-                title = search('title', articleNode)
-                if (!title) {
-                    title = ""
+    got(url)
+        .then(async (response) => {
+            const dom = new JSDOM(response.body, {
+                    resources: 'usable',
+                    includeNodeLocations: true,
+                }),
+                articleNode = dom.window.document.querySelector(selector)
+            if (articleNode) {
+                //if article element found, get its HTML content
+                const html = articleNode.innerHTML
+                let markdown = ''
+                if (html) {
+                    markdown = html2markdown.translate(html)
                 }
-            }
-            let image = null;
-            if (!ignoreImage) {
-                if (imageUrl) {
-                    //use image url that is provided
-                    image = imageUrl;
-                } else {
-                    //Get cover image of the article
-                    if (imageSelector) {
-                        //get image using selector specified
-                        image = dom.window.document.querySelector(imageSelector)
-                        if (image) {
-                            image = image.getAttribute('src')
-                        }
+                //check if title is set in the command line arguments
+                if (!title) {
+                    //get title of article
+                    title = search('title', articleNode)
+                    if (!title) {
+                        title = ''
+                    }
+                }
+                let image = null
+                if (!ignoreImage) {
+                    if (imageUrl) {
+                        //use image url that is provided
+                        image = imageUrl
                     } else {
-                        //check if there's a default image selector in config
-                        imageSelector = configstore.get('imageSelector')
+                        //Get cover image of the article
                         if (imageSelector) {
-                            image = dom.window.document.querySelector(imageSelector)
+                            //get image using selector specified
+                            image =
+                                dom.window.document.querySelector(imageSelector)
                             if (image) {
                                 image = image.getAttribute('src')
                             }
                         } else {
-                            image = search('image', articleNode)
+                            //check if there's a default image selector in config
+                            imageSelector = configstore.get('imageSelector')
+                            if (imageSelector) {
+                                image =
+                                    dom.window.document.querySelector(
+                                        imageSelector
+                                    )
+                                if (image) {
+                                    image = image.getAttribute('src')
+                                }
+                            } else {
+                                image = search('image', articleNode)
+                            }
+                        }
+                        //check if image is dataurl
+                        if (image && isDataURL(image)) {
+                            const response = await uploadToCloudinary(image)
+                            image = response.url
                         }
                     }
-                    //check if image is dataurl
-                    if (image && isDataURL(image)) {
-                        const response = await uploadToCloudinary(image)
-                        image = response.url
+                }
+                chosenPlatforms.forEach((platform) => {
+                    switch (platform) {
+                        case 'dev':
+                            loading.message(`Posting article to dev.to...`)
+                            postToDev(
+                                title,
+                                markdown,
+                                url,
+                                image,
+                                public,
+                                afterPost
+                            ).catch(handleError)
+                            break
+                        case 'hashnode':
+                            loading.message(`Posting article to Hashnode...`)
+                            postToHashnode(
+                                title,
+                                markdown,
+                                url,
+                                image,
+                                public,
+                                afterPost
+                            ).catch(handleError)
+                            break
+                        case 'medium':
+                            loading.message(`Posting article to Medium...`)
+                            postToMedium(
+                                title,
+                                markdown,
+                                url,
+                                public,
+                                afterPost
+                            ).catch(handleError)
                     }
-                }
+                })
+            } else {
+                throw new Error('No articles found in the URL.')
             }
-            chosenPlatforms.forEach((platform) => {
-                switch (platform) {
-                    case 'dev':
-                        loading.message(`Posting article to dev.to...`)
-                        postToDev(title, markdown, url, image, public, afterPost)
-                        .catch(handleError)
-                        break
-                    case 'hashnode':
-                        loading.message(`Posting article to Hashnode...`)
-                        postToHashnode(title, markdown, url, image, public, afterPost)
-                        .catch(handleError)
-                        break;
-                    case 'medium':
-                        loading.message(`Posting article to Medium...`)
-                        postToMedium(title, markdown, url, public, afterPost)
-                        .catch(handleError)
-                }
-            })
-        } else {
-            throw new Error('No articles found in the URL.')
-        }
-    })
-    .catch(handleError)
+        })
+        .catch(handleError)
 }
 
 function handleError(err) {
@@ -156,10 +188,14 @@ function handleError(err) {
 /**
  * Function to run after posting on a platform is done
  */
-function afterPost({success, url = '', platform = '', public = false}) {
+function afterPost({ success, url = '', platform = '', public = false }) {
     if (success) {
         console.log(
-            displaySuccess(`Article ${public ? 'published' : 'added to drafts'} on ${platform} at ${url}`)
+            displaySuccess(
+                `Article ${
+                    public ? 'published' : 'added to drafts'
+                } on ${platform} at ${url}`
+            )
         )
     }
     platformsPosted++
@@ -169,28 +205,35 @@ function afterPost({success, url = '', platform = '', public = false}) {
 /**
  * If the number of platforms posted on is complete stop the loading
  */
-async function checkIfShouldStopLoading () {
+async function checkIfShouldStopLoading() {
     if (platformsPosted === chosenPlatforms.length) {
         loading.stop()
     }
 }
 
 /**
- * 
+ *
  * @param {string} type Type to search for. Can be either title or
  * @param {HTMLElement} node element to search in
- * @returns 
+ * @returns
  */
-function search (type, node) {
-    if ((type === 'title' && (node.tagName === 'H1' || node.tagName === 'H2' || node.tagName === 'H3' || 
-            node.tagName === 'H4' || node.tagName === 'H5' || node.tagName === 'H6')) || 
-        (type === "image" && node.tagName === 'IMG')) {
+function search(type, node) {
+    if (
+        (type === 'title' &&
+            (node.tagName === 'H1' ||
+                node.tagName === 'H2' ||
+                node.tagName === 'H3' ||
+                node.tagName === 'H4' ||
+                node.tagName === 'H5' ||
+                node.tagName === 'H6')) ||
+        (type === 'image' && node.tagName === 'IMG')
+    ) {
         return type === 'title' ? node.textContent : node.getAttribute('src')
     }
 
     if (node.childNodes && node.childNodes.length) {
         const childNodes = node.childNodes
-        for(let i = 0; i < childNodes.length; i++) {
+        for (let i = 0; i < childNodes.length; i++) {
             const title = search(type, childNodes.item(i))
             if (title) {
                 return title
