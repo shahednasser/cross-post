2fc986d545991532158357c0cd2112192a95cf27
HarshaVardhanNakkina <harshavardhan.n@yahoo.com>
2022-03-13 15:54:15 +0530

    📝 add help text, examples for reset commands

--- src/commands/config/config-reset.js
+++ src/commands/config/config-reset.js
@@ -4,6 +4,16 @@ const reset = new Command();
 
 reset.name('reset')
   .description('reset configuration for a given platform(s)')
+  .addHelpText('after', `
+Example:
+    # to reset dev.to configuration
+    $ cross-post config reset dev
+
+    # to reset all non-platform configuration
+    $ cross-post config reset all
+        (or just)
+    $ cross-post config reset
+  `)
   .executableDir('./reset')
   .command('dev', 'reset configuration for dev.to')
   .command('medium', 'reset configuration for medium.com')
