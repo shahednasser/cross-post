6c4486256253faae744a374f7e8d41a7652c82b1
HarshaVardhanNakkina <harshavardhan.n@yahoo.com>
2022-03-13 15:03:29 +0530

    ✨ reset command for medium platform

--- src/commands/config/config-reset.js
+++ src/commands/config/config-reset.js
@@ -5,6 +5,7 @@ const reset = new Command();
 reset.name('reset')
   .description('reset configuration for a given platform(s)')
   .executableDir('./reset')
-  .command('dev', 'reset configuration for dev.to');
+  .command('dev', 'reset configuration for dev.to')
+  .command('medium', 'reset configuration for medium.com');
 
 reset.parse();
